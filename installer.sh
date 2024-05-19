3022ea0823cf15eeee90dd1cc241dd73e0616a6f
hamletsargsyan <hamlets849@gmail.com>
2024-05-19 13:44:31 +0400

    Правки

--- installer.sh
+++ installer.sh
@@ -11,11 +11,9 @@ else
 fi
 
 packages=(
-    git
     python3
-    python3-pip
     rust
-    batcat
+    bat
     bpython
     neofetch
     neovim
@@ -24,39 +22,30 @@ packages=(
     curl
     wget
     golang
+    tree
+    zsh
 )
 
 pip_packages=(
-    ruff
     black
     pyright
 )
 
-log() {
-    local log_level=$1
-    shift
-    local message=$@
-    local timestamp=$(date +'%Y-%m-%d %H:%M:%S')
-    local log_file="dotfiles_installer.log"
-    
-    case $log_level in
-        "INFO") echo -e "[${timestamp}] [INFO] ${message}" | tee -a "$log_file" ;;
-        "WARNING") echo -e "[${timestamp}] [WARNING] ${message}" >&2 | tee -a "$log_file" ;;
-        "ERROR") echo -e "[${timestamp}] [ERROR] ${message}" >&2 | tee -a "$log_file" ;;
-        *) echo -e "[${timestamp}] [UNKNOWN] ${message}" >&2 | tee -a "$log_file" ;;
-    esac
-}
+
+if [ ! -n $TERMUX_VERSION];
+then
+    pip install ruff
+    curl -sSL https://install.python-poetry.org | python3 -
+
+fi
 
 
-log INFO "Start installing packages"
 for package in "${packages[@]}"
 do
-    log INFO "Installing $package"
     $SUDO_COMMAND $PKG_MANAGER install $package
 done
 
 
-log INFO "Start installing packages with pip"
 for package in "${pip_packages[@]}"
 do
     log INFO "Installing $package"
@@ -64,16 +53,8 @@ do
 done
 
 
-log INFO "Start installing packages with from scripts"
 
-log INFO "Installing poetry"
-curl -sSL https://install.python-poetry.org | python3 -
-
-log INFO "Installing zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-log INFO "changing shell"
-chsh -s $(which zsh)
+chsh --shell $(which zsh)
 
 # ---------------------------------------------------------------------------- #
 
