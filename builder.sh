8d3b3d869152e04eb9c04958c042c8a6352fa0bc
hamletsargsyan <hamlets849@gmail.com>
2024-05-22 22:04:44 +0400

    Правки

--- builder.sh
+++ builder.sh
@@ -3,7 +3,7 @@
 SUDO_COMMAND=
 PKG_MANAGER=
 
-if [ -n "$TERMUX_VERSION" ]; then
+if [[ -n "$TERMUX_VERSION" ]]; then
     termux-change-repo
     PKG_MANAGER="pkg"
 else
@@ -34,31 +34,23 @@ pip_packages=(
     pyright
 )
 
-
 $SUDO_COMMAND $PKG_MANAGER update -y
 
-if [ ! -n $TERMUX_VERSION ];
-then
+if [[ -z "$TERMUX_VERSION" ]]; then
     pip install ruff
     curl -sSL https://install.python-poetry.org | python3 -
-
 fi
 
-
-for package in "${packages[@]}"
-do
-    yes | $SUDO_COMMAND $PKG_MANAGER install $package
+for package in "${packages[@]}"; do
+    yes | $SUDO_COMMAND $PKG_MANAGER install "$package"
 done
 
-
-for package in "${pip_packages[@]}"
-do
-    pip install $package
+for package in "${pip_packages[@]}"; do
+    pip install "$package"
 done
-
 
 RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
+chsh -s "$(which zsh)"
 
 cp zshrc ~/.zshrc
 cp p10k.zsh ~/.p10k.zsh
@@ -67,16 +59,15 @@ mkdir -p ~/.config
 mkdir -p ~/repos
 mkdir -p ~/tests
 
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
 git clone https://github.com/HamletSargsyan/astronvim_config ~/.config/nvim
 
 curl https://getcroc.schollz.com | bash
 
-if [[ $TERMUX_VERSION ]]; then
+if [[ -n "$TERMUX_VERSION" ]]; then
     rm -rfv ~/.termux/
     cp -r ./termux/ ~/.termux/
 fi
